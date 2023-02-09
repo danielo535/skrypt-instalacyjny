f3cb7844f43842893ec74b0fcec04206c3b75ae6
danielo535 <79422069+danielo535@users.noreply.github.com>
2023-02-09 20:31:11 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,59 +1,49 @@
 #!/bin/bash
 
-echo "Witaj w instalatorze Pterodactyl Wings i PHPMyAdmin"
+echo "Witaj w instalatorze Pterodactyl, Wings i PHPMyAdmin"
 echo "Wybierz opcję instalacji:"
-echo "1. Instalacja Pterodactyl"
-echo "2. Instalacja Wings"
-echo "3. Instalacja pHpMyAdmin"
-echo "4. Instalacja wszystkiego"
-echo "5. Wyjdz"
+echo "1. Instalacja tylko Pterodactyl"
+echo "2. Instalacja tylko Wings"
+echo "3. Instalacja tylko PHPMyAdmin"
+echo "4. Instalacja Pterodactyl i Wings"
+echo "5. Instalacja Pterodactyl i PHPMyAdmin"
+echo "6. Instalacja Wings i PHPMyAdmin"
+echo "7. Instalacja Pterodactyl, Wings i PHPMyAdmin"
 read option
 
 if [ $option == "1" ]
 then
   echo "Rozpoczynam instalację Pterodactyl"
-  sudo apt-get update
-  sudo apt-get install curl -y
-  curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-  sudo apt-get install nodejs -y
-  curl -Lo install.sh https://github.com/pterodactyl/panel/releases/download/v0.7.17/install.sh
-  sudo bash install.sh
-  echo "Panel Pterodactyl został zainstalowany pomyślnie!"
-
-if [ $option == "2" ]
-then
-  sudo apt-get update
-  sudo apt-get install phpmyadmin -y
-  sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
-  sudo apt-get install php8.0-fpm php8.0-mysql php8.0-mysqli
-  sudo apt-get install php8.0-xml
-  sudo apt-get install php8.0-mbstring php8.0-zip php8.0-gd
-  sudo systemctl restart php8.0-fpm
-  sudo systemctl restart nginx
-  echo "phpMyAdmin został zainstalowany pomyślnie!"
-
-if [ $option == "3" ]
-then
-  sudo apt-get update
-  sudo apt-get install phpmyadmin -y
-  sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
-  sudo apt-get install php8.0-fpm php8.0-mysql php8.0-mysqli
-  sudo apt-get install php8.0-xml
-  sudo apt-get install php8.0-mbstring php8.0-zip php8.0-gd
-  sudo systemctl restart php8.0-fpm
-  sudo systemctl restart nginx
-  echo "phpMyAdmin został zainstalowany pomyślnie!"
-
-if [ $option == "4" ]
-then
-  echo "Rozpoczynam instalację Pterodactyl Wings"
-  sudo apt-get update
+  # tutaj możesz dodać komendy do instalacji Pterodactyl
   echo "Instalacja zakończona pomyślnie"
-
-if [ $option == "5" ]
+elif [ $option == "2" ]
 then
-  echo "Rozpoczynam instalację Pterodactyl Wings"
-  exit 1
+  echo "Rozpoczynam instalację Wings"
+  # tutaj możesz dodać komendy do instalacji Wings
   echo "Instalacja zakończona pomyślnie"
-
+elif [ $option == "3" ]
+then
+  echo "Rozpoczynam instalację PHPMyAdmin"
+  # tutaj możesz dodać komendy do instalacji PHPMyAdmin
+  echo "Instalacja zakończona pomyślnie"
+elif [ $option == "4" ]
+then
+  echo "Rozpoczynam instalację Pterodactyl i Wings"
+  # tutaj możesz dodać komendy do instalacji Pterodactyl i Wings
+  echo "Instalacja zakończona pomyślnie"
+elif [ $option == "5" ]
+then
+  echo "Rozpoczynam instalację Pterodactyl i PHPMyAdmin"
+  # tutaj możesz dodać komendy do instalacji Pterodactyl i PHPMyAdmin
+  echo "Instalacja zakończona pomyślnie"
+elif [ $option == "6" ]
+then
+  echo "Rozpoczynam instalację Wings i PHPMyAdmin"
+  # tutaj możesz dodać komendy do instalacji Wings i PHPMyAdmin
+  echo "Instalacja zakończona pomyślnie"
+elif [ $option == "7" ]
+then
+  echo "Rozpoczynam instalację Pterodactyl, Wings i PHPMyAdmin"
+  # tutaj możesz
+  echo "Instalacja zakończona pomyślnie"
 fi
