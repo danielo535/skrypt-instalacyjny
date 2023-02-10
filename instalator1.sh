7bb2d9ef96b22ec772e3a517676b4d596fb047d9
danielo535 <79422069+danielo535@users.noreply.github.com>
2023-02-10 14:12:15 +0100

    Update instalator1.sh

--- instalator1.sh
+++ instalator1.sh
@@ -1,28 +1,22 @@
-#!/bin/bash
+### Options ###
 
-echo "Wybierz opcję instalacji:"
-echo "1) Pełna instalacja"
-echo "2) Instalacja podstawowa"
-echo "3) Anulowanie instalacji"
-read -r option
-
-case $option in
-    1)
-        echo "Uruchamianie pełnej instalacji..."
-        #test
-        ;;
-    2)
-        echo "Uruchamianie instalacji podstawowej..."
-        #test
-        ;;
-    3)
-        echo "Anulowanie instalacji."
-        exit
-        ;;
-    *)
-        echo "Nieprawidłowa opcja."
-        exit
-        ;;
-esac
-
-echo "Instalacja zakończona."
+options(){
+    output "Please select your installation option:"
+    warning "[1] Install Panel. | Installs latest version of Pterodactyl Panel"
+    warning "[2] Install Wings. | Installs latest version of Pterodactyl Wings."
+    warning "[3] Install PHPMyAdmin. | Installs PHPMyAdmin. (Installs using NGINX)"
+    read -r option
+    case $option in
+        1 ) option=1
+            start
+            ;;
+        2 ) option=2
+            startwings
+            ;;
+        3 ) option=3
+            startphpmyadmin
+            ;;
+        * ) output ""
+            output "Please enter a valid option from 1-10"
+    esac
+}
