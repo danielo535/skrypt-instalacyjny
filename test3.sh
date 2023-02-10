ae0e192bf4868614623c94ba214c702e9f66cf80
danielo535 <79422069+danielo535@users.noreply.github.com>
2023-02-10 14:18:29 +0100

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -1,9 +1,9 @@
 ### Options ###
 
     echo "Please select your installation option:"
-    echo "[1] Install Panel. | Installs latest version of Pterodactyl Panel"
-    echo "[2] Install Wings. | Installs latest version of Pterodactyl Wings."
-    echo "[3] Install PHPMyAdmin. | Installs PHPMyAdmin. (Installs using NGINX)"
+    echo "[1] Install Panel."
+    echo "[2] Install Wings."
+    echo "[3] Install PHPMyAdmin. | (Installs using NGINX)"
     read -r option
     case $option in
         1 ) option=1
